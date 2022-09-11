582ee755e6ca9ecc9bfbcf7a011e19e59bad6570
code <33802433+codeacade@users.noreply.github.com>
2022-09-11 22:55:59 +0100

    99% dome!!

--- ct8React/app.js
+++ ct8React/app.js
@@ -1,26 +1,76 @@
-// recreating ct8 node / React enviromnment: wiki.mydevil.net/React
-// no more hardcoded data - now readFile from db.json
-
 const express = require('express');
 const app = express();
 app.use(express.json())  // do magic with "put", "post"
-
 const fs = require('fs');
-fs.readFile('./db.json', (err, data) => {
-    toDoList = JSON.parse(data);
-})
+
 
 app.get('/api/', (req, res) => {
-    res.json( toDoList );
-    console.log("GET - " + new Date())
+
+    fs.readFile('./db.json', (err, data) => {
+        toDoList = JSON.parse(data);
+
+        res.json( toDoList );
+        console.log("GET - " + new Date())
+    })
 });
 
-app.put('/api/:pars', (req, res) => {
-    console.log(req.body);   // MUST app.use(express.json())  
+
+app.put('/api/:x', (req, res) => {
+    putBody = req.body;    // MUST app.use(express.json()) 
+
+    fs.readFile('./db.json', (err, data) => {
+        toDoList = JSON.parse(data);
+
+        putIndex = toDoList.elements.findIndex( obj => obj.id == putBody.id )
+
+        if(putIndex>-1){
+            toDoList.elements[putIndex] = putBody;
+            fs.writeFile('./db.json', JSON.stringify(toDoList), console.error );
+        }
+    
+        console.log("PUT - " + new Date())
+    })
+    res.status(204).send();  // React waits for that!
 });
+
 
 app.post('/api/', (req, res) => {
-    console.log(req.body);   // MUST app.use(express.json())  
+    postBody = req.body;   // MUST app.use(express.json())
+
+    fs.readFile('./db.json', (err, data) => {
+        toDoList = JSON.parse(data);
+
+        if(postBody.id && postBody.title){
+            newListObject = { id: postBody.id, title: postBody.title, isDone: postBody.isDone }
+            toDoList.elements = [newListObject].concat(toDoList.elements);
+            fs.writeFile('./db.json', JSON.stringify(toDoList), console.error );
+        }
+    
+        console.log("POST - " + new Date())
+    })
+    res.status(204).send();  // React waits for that!
 });
+
+
+app.delete('/api/:id', (req, res) => {
+    deleteId = req.params.id;    // MUST app.use(express.json()) 
+
+    fs.readFile('./db.json', (err, data) => {
+        toDoList = JSON.parse(data);
+        deleteIndex = toDoList.elements.findIndex( obj => obj.id == deleteId )
+
+        if(deleteIndex>-1) {
+            filteredList = toDoList.elements.filter( obj => obj.id != deleteId);
+            fs.writeFile('./db.json', 
+                JSON.stringify({elements: filteredList}), 
+                console.error 
+            );
+        }
+
+        console.log("DELETE - " + new Date())
+    })
+    res.status(204).send();  // React waits for that!
+});
+
 
 app.listen(3000);
