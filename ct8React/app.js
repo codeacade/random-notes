df4efcd22d8461ac28edd01f3d93f03515ecc266
code <33802433+codeacade@users.noreply.github.com>
2022-09-11 14:18:05 +0100

    no more hardcoded data - now readFile from db.json

--- ct8React/app.js
+++ ct8React/app.js
@@ -1,34 +1,16 @@
 // recreating ct8 node / React enviromnment: wiki.mydevil.net/React
+// no more hardcoded data - now readFile from db.json
 
 const express = require('express');
 const app = express();
+const fs = require('fs');
+
+fs.readFile('./db.json', (err, data) => {
+    toDoList = JSON.parse(data);
+})
 
 app.get('/api', (req, res) => {
-    res.json({
-    "elements": [
-        {
-            "id": 223344,
-            "title": "Buy nuts and crisps!",
-            "isDone": true
-        },
-        {
-            "id": 223313,
-            "title": "Charge tablet",
-            "isDone": false
-        },
-        {
-            "id": 171738,
-            "title": "Check hosting",
-            "isDone": false
-        },
-        {
-            "id": 36948,
-            "title": "Eat crisps!",
-            "isDone": true
-        }
-        ]
-
-    });
+    res.json( toDoList );
 });
 
 app.listen(3000);
