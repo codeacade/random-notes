6df168542e98a58911102b4ccea376e2583f24cf
code <33802433+codeacade@users.noreply.github.com>
2022-09-11 17:43:30 +0100

    app.use(express.json()) to use PUT/POST

--- ct8React/app.js
+++ ct8React/app.js
@@ -3,14 +3,24 @@
 
 const express = require('express');
 const app = express();
-const fs = require('fs');
+app.use(express.json())  // do magic with "put", "post"
 
+const fs = require('fs');
 fs.readFile('./db.json', (err, data) => {
     toDoList = JSON.parse(data);
 })
 
-app.get('/api', (req, res) => {
+app.get('/api/', (req, res) => {
     res.json( toDoList );
+    console.log("GET - " + new Date())
+});
+
+app.put('/api/:pars', (req, res) => {
+    console.log(req.body);   // MUST app.use(express.json())  
+});
+
+app.post('/api/', (req, res) => {
+    console.log(req.body);   // MUST app.use(express.json())  
 });
 
 app.listen(3000);
